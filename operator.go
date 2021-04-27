0fd0451106bc494e02a2fd824e9312b2362cba76
marchella2 <marchella.putris2@gmail.com>
2021-04-27 22:25:18 +0700

    3. Operator Matematika dan Logika

---
+++ operator.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Penjumlahan
+	fmt.Println(1 + 1)
+
+	// Pengurangan
+	fmt.Println(3 - 2)
+
+	// Perkalian
+	fmt.Println(5 * 2)
+
+	// Pembagian
+	fmt.Println(10 / 2)
+
+	// Pembagian desimal
+	fmt.Println(10.0 / 3.0)
+
+	// Sisa bagi atau modulus
+	fmt.Println(10 % 3)
+
+	// Operator logika
+	fmt.Println(true && false)  // && memiliki arti and
+	fmt.Println(false || false) // || memiliki arti or
+	fmt.Println(!false)         // ! memiliki arti not
+}
