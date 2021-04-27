884306f8150f547115531a8c66b799765138c7c6
marchella2 <marchella.putris2@gmail.com>
2021-04-27 22:00:57 +0700

    2. Tipe Data

---
+++ datatype.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Tipe data number
+	fmt.Println(1)
+	fmt.Println(1.5) // Untuk membuat bilangan desimal menggunakan . bukan ,
+
+	// Tipe data string
+	fmt.Println("Marchella")
+	fmt.Println(`Putri`) // Untuk tipe data string bisa menggunakan `` atau ""
+
+	// Tipe data boolean
+	fmt.Println(true)
+	fmt.Println(true && false)  // && memiliki arti and
+	fmt.Println(false || false) // || memiliki arti or
+	fmt.Println(!false)         // ! memiliki arti not
+}
