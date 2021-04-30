894779efb394bf622d5ab668fdc359fda632cbf5
marchella2 <marchella.putris2@gmail.com>
2021-04-30 13:55:57 +0700

    12. Array

---
+++ array.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [5]string
+
+	names[0] = "Marchella"
+	names[1] = "Putri"
+	names[2] = "Sannie"
+	names[3] = "Test"
+	names[4] = "Halo"
+
+	// Menampilkan dengan menggunakan perulangan
+	for i := 0; i < len(names); i++ { // len(names) berfungsi untuk mengecek berapa panjang dari array
+		fmt.Println(names[i])
+	}
+
+	// Membuat perulangan kedua (cara simple)
+	for _, value := range names { //index = nama variable, nama variable bebas
+		// fmt.Println("index", index, "=", value)
+		fmt.Println(value) // Jika ingin memanggil value nya
+		// Jika variabel index tidak digunakan bisa diganti dengan _
+	}
+
+	// fmt.Println(names) // Memanggil semua data array
+	// fmt.Println(names[0]) // Memanggil data array yang dipilih
+}
