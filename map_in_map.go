2b33a3a576d19df5113ebc7bfbea35ace77ebe11
marchella2 <marchella.putris2@gmail.com>
2021-04-30 23:15:40 +0700

    15. Map dalam map (Finish)

---
+++ map_in_map.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	// mahasiswa := map[string]string{
+	// 	"1001": "Marchella",
+	// 	"1002": "Putri",
+	// 	"1003": "Sannie",
+	// }
+
+	// Pola map dalam map 
+	mahasiswa := map[string]map[string]string{
+		"1001" : map[string]string{
+			"name" : "Marchella",
+			"address" : "Indonesia",
+			"gender" : "Female",
+		},
+
+		"1002" : map[string]string{
+			"name" : "Sania",
+			"address" : "Indonesia",
+			"gender" : "Female",
+		},
+
+		"1003" : map[string]string{
+			"name" : "Arga",
+			"address" : "Indonesia",
+			"gender" : "Male",
+		},
+	}
+
+	// Cara delete map
+	delete(mahasiswa, "1003")
+
+	fmt.Println(mahasiswa)
+	// fmt.Println(mahasiswa["1001"]["name"])
+}
